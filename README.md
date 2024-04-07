d0e3297a7f48917e4c9bc2275e829060e56ab8c2
Jachin <1151530342@qq.com>
2024-04-07 11:30:16 +0800

    feat: calc

---
+++ README.md
@@ -0,0 +1,2 @@
+A vite plugin as a solution for the problem in the calculation precision of the floating points numbers.
+
--- src/calc.js
+++ src/calc.js
@@ -3,34 +3,75 @@ function isNumber (variable) {
   return typeof variable === 'number'
 }
 
+function insertString (str, index, insertStr) {
+  return str.slice(0, index) + insertStr + str.slice(index);
+}
+
+function formatResult (res, decimalLen) {
+  if (decimalLen === 0) return res
+  const isNagitive = res < 0
+  let str = Math.abs(res).toString()
+  decimalLen += str.split('.')[1]?.length || 0
+  str = str.replace('.', '')
+  if (decimalLen > str.length) {
+    str = '0'.repeat(decimalLen - str.length) + str
+  }
+  if (decimalLen > 0) {
+    return Number((isNagitive ? '-' : '') + insertString(str, -decimalLen, '.'))
+  } else {
+    return Number((isNagitive ? '-' : '') + str + '0'.repeat(-decimalLen))
+  }
+}
+
+function parseCalculateNumber (left, right, decimalAlign = false) {
+  const leftStr = left.toString()
+  const rightStr = right.toString()
+  const leftDecimalLength = leftStr.split('.')[1]?.length || 0
+  const rightDecimalLength = rightStr.split('.')[1]?.length || 0
+  const maxDecimalLength = Math.max(leftDecimalLength, rightDecimalLength)
+  const totalDecimalLength = leftDecimalLength + rightDecimalLength
+  const diffDecimalLength = leftDecimalLength - rightDecimalLength
+  const leftInt = Number(leftStr.replace('.', '') + (decimalAlign ? '0'.repeat(maxDecimalLength - leftDecimalLength) : ''))
+  const rightInt = Number(rightStr.replace('.', '') + (decimalAlign ? '0'.repeat(maxDecimalLength - rightDecimalLength) : ''))
+  return {
+    leftInt,
+    rightInt,
+    maxDecimalLength,
+    totalDecimalLength,
+    diffDecimalLength,
+  }
+}
+
 export function add (left, right) {
   if (!isNumber(left) || !isNumber(right)) {
     return left + right
   } else if (Number.isInteger(left) || Number.isInteger(right)) {
     return left + right
   } else {
-    // 需要替换为高精度计算
-    return left + right
+    const {
+      leftInt,
+      rightInt,
+      maxDecimalLength,
+    } = parseCalculateNumber(left, right, true)
+    return formatResult(leftInt + rightInt, maxDecimalLength)
   }
 }
 export function sub (left, right) {
-  if (!isNumber(left) || !isNumber(right)) {
-    return left - right
-  } else if (Number.isInteger(left) || Number.isInteger(right)) {
-    return left - right
-  } else {
-    // 需要替换为高精度计算
-    return left - right
-  }
+  return add(left, -right)
 }
+
 export function mul (left, right) {
   if (!isNumber(left) || !isNumber(right)) {
     return left * right
-  } else if (Number.isInteger(left) || Number.isInteger(right)) {
+  } else if (Number.isInteger(left) && Number.isInteger(right)) {
     return left * right
   } else {
-    // 需要替换为高精度计算
-    return left * right
+    const {
+      leftInt,
+      rightInt,
+      totalDecimalLength,
+    } = parseCalculateNumber(left, right)
+    return formatResult(leftInt * rightInt, totalDecimalLength)
   }
 }
 export function div (left, right) {
@@ -39,7 +80,11 @@ export function div (left, right) {
   } else if (Number.isInteger(left) || Number.isInteger(right)) {
     return left / right
   } else {
-    // 需要替换为高精度计算
-    return left / right
+    const {
+      leftInt,
+      rightInt,
+      diffDecimalLength,
+    } = parseCalculateNumber(left, right)
+    return formatResult(leftInt / rightInt, diffDecimalLength)
   }
 }
